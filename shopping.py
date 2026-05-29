d2162b6fc4c760ff7a2635cebf09d590d1327776
Adcom Usługi Informatyczne Adrian Kapuścik <adrian1230@wp.pl>
2026-05-29 11:46:07 +0200

    Adding a product form
    new product form added

--- shopping.py
+++ shopping.py
@@ -21,13 +21,17 @@ def get_idx():
     ids = [ tree.item(i)['values'][0] for i in tree.get_children() ]
     return max(ids, default=0) + 1
 
+def add_product():
+    new_product_form()
+
 def add_new_product():
-    if not entry_product.get():
+    if not add_product_entry.get():
         return
 
-    tree.insert('', 'end', values=(get_idx(), entry_product.get()))
+    tree.insert('', 'end', values=(get_idx(), add_product_entry.get()))
     save_to_json()
-    entry_product.delete(0, tk.END)
+    add_product_entry.delete(0, tk.END)
+    window_new_product.destroy()
 
 def save_to_json():
     data = []
@@ -85,10 +89,11 @@ def verify_user():
         messagebox.showerror('Błąd logowania', f'Błędny login lub hasło')
 
 def product_form():
-    global tree, entry_product
+    global tree, entry_product, window
     
     window = tk.Tk()
     window.title('Lista zakupów')
+    window.configure(background=BG_COLOR)
 
     SCREEN_WIDTH = window.winfo_screenwidth()
     SCREEN_HEIGHT = window.winfo_screenheight()
@@ -106,7 +111,7 @@ def product_form():
     entry_product.pack(pady=10)
 
     # przyciski
-    product_add = tk.Button(window, text="Dodaj produkt", command=add_new_product)
+    product_add = tk.Button(window, text="Dodaj produkt", command=add_product)
     product_add.pack(pady=5)
 
     product_del = tk.Button(window, text="Usuń produkt", command=remove_product)
@@ -122,6 +127,36 @@ def product_form():
 
     read_from_json()
     window.mainloop()
+    
+def new_product_form():
+    global window_new_product, add_product_entry
+    
+    window_new_product = tk.Toplevel(window)
+    window_new_product.title('Nowy product')
+    window_new_product.configure(background=BG_COLOR)
+
+    SCREEN_WIDTH = window_new_product.winfo_screenwidth()
+    SCREEN_HEIGHT = window_new_product.winfo_screenheight()
+
+    WIDTH = 400
+    HEIGHT = 500
+
+    x = (SCREEN_WIDTH - WIDTH) // 2
+    y = (SCREEN_HEIGHT - HEIGHT) // 2
+
+    window_new_product.geometry(f'{WIDTH}x{HEIGHT}+{x}+{y}')
+    
+    head_lbl = tk.Label(window_new_product, text='Nowy produkt', font=tkfont.Font(size=30, weight='bold'), bg=BG_COLOR)
+    head_lbl.pack(pady=(30,10))
+    
+    add_product_lbl = tk.Label(window_new_product, text='Product name:', anchor='w', padx=75, bg=BG_COLOR)
+    add_product_lbl.pack(fill='x')
+    
+    add_product_entry = tk.Entry(window_new_product, width=40)
+    add_product_entry.pack(pady=5)
+    
+    add_product_btn = tk.Button(window_new_product, text='Dodaj', width=35, border=1, command=add_new_product)
+    add_product_btn.pack(fill='x', ipady=5, padx=75, pady=20)
     
 def login_form():
     
